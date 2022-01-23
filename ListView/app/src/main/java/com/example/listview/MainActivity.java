8db078c7ff02cebe0d44a7c495c8a1c5eda8dd1e
JeUnSong <94476600+JeUnSong@users.noreply.github.com>
2022-01-23 23:15:55 +0900

    ListView를 이용한 리스트 만들기
    + ArrayAdapter

---
+++ ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.list);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("자바");
+        data.add("스위프트");
+        data.add("파이썬");
+        adapter.notifyDataSetChanged();
+    }
+}
