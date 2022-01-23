032cbc46514018c783ed2fdfa287fc6a7e6a3545
JeUnSong <94476600+JeUnSong@users.noreply.github.com>
2022-01-23 19:45:06 +0900

    수치변경

--- Button/app/src/main/java/com/example/button/MainActivity.java
+++ Button/app/src/main/java/com/example/button/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
 
     EditText ed_id;
     Button but_test;
+    EditText ed_ps;
+    Button but_test2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
         ed_id = findViewById(R.id.ed_id);
         but_test = findViewById(R.id.but_test);
+        ed_ps = findViewById(R.id.ed_ps);
+        but_test2 = findViewById(R.id.but_test2);
 
         but_test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 ed_id.setText("제운");
+                ed_ps.setText("*****");
             }
         });
 
